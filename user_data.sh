9571bffe2c59f6a025a012d1cb21f198fa9c87f9
Florian Buchmeier <florian.buchmeier@audi.de>
2019-08-08 14:04:57 +0200

    reverted to aws s3 cp instead of sync since sync requires additional permissions

--- user_data.sh
+++ user_data.sh
@@ -88,7 +88,7 @@ cat > /usr/bin/bastion/sync_s3 << 'EOF'
 # Copy log files to S3 with server-side encryption enabled.
 # Then, if successful, delete log files that are older than a day.
 LOG_DIR="/var/log/bastion/"
-aws s3 sync /var/log s3://${bucket_name}/logs/ --sse --region ${aws_region} && find $LOG_DIR* -mtime +1 -exec rm {} \;
+aws s3 cp /var/log s3://${bucket_name}/logs/ --sse --region ${aws_region} --recursive && find $LOG_DIR* -mtime +1 -exec rm {} \;
 
 EOF
 
